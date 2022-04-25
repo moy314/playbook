90633ecc25fb1ecdc81f98d77a31e339f4fc07cf
Moises Sanchez <moy5921@gmail.com>
2022-04-25 12:48:23 -0500

    agregando practica 1 semana3

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const moy = new MissionCommander("moy")
+const vale = new MissionCommander("vale")
+
+
+
+console.log(carlo.name)
+console.log(moy.name)
+console.log(vale.name)
