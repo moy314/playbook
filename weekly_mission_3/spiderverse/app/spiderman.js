ae25c8269f5df58ff9ce29aa22f515623c4d5096
Moises Sanchez <moy5921@gmail.com>
2022-04-26 14:27:05 -0500

    agregando clase spiderman y pruebas

---
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -0,0 +1,15 @@
+class Spiderman{
+
+    constructor(nombre,edad,actor,numPeliculas,estudioCine){
+
+        this.nombre = nombre;
+        this.edad = edad;
+        this.actor = actor;
+        this.numPeliculas = numPeliculas;
+        this.estudioCine = estudioCine;
+
+    }
+
+
+}
+module.exports = Spiderman
